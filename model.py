1c0983b02dc04dafa3a5bba02a229247c453daa3
Sneha U <74255377+Snehau259@users.noreply.github.com>
2022-06-18 12:02:42 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, Flatten, Multiply, Dropout
+from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, Flatten, Multiply
 from tensorflow.keras.optimizers import Adam
 import numpy as np 
 import matplotlib.pyplot as plt
@@ -13,62 +13,24 @@ import os
 import seaborn as sns
 from keras.applications.vgg16 import VGG16
 from tensorflow.keras.applications.resnet50 import ResNet50
+from keras import models
+from keras import layers
 
 def build_model(im_shape, vocab_size, num_answers, big_model):
   # The CNN
   im_input = Input(shape=im_shape)
-#   x1 = Conv2D(8, 3, padding='same')(im_input) 8-no of filters, 3-kernel/filter-size
-#   x1 = MaxPooling2D()(x1)
-#   x1 = Conv2D(16, 3, padding='same')(x1)
-#   x1 = MaxPooling2D()(x1)
-#   if big_model:
-#     x1 = Conv2D(32, 3, padding='same')(x1)
-#     x1 = MaxPooling2D()(x1)
-#   x1 = Flatten()(x1)
-#   #Load model wothout classifier/fully connected layers
   x1 = VGG16(weights='imagenet', include_top=False, input_shape=(250, 250, 3))(im_input)
-  x1 = Dropout(0.5)(x1)
-  x1 = BatchNormalization(axis=1)(x1)
-
-  #image_input = Input(shape=(250, 250, 3))
-  #x1 = ResNet50(input_tensor=image_input, include_top=False,weights='imagenet')
-  #print("model type=",type(x1)," Size=",x1.shape)
-  #Make loaded layers as non-trainable. This is important as we want to work with pre-trained weights
-#   for layer in x1.layers:
-#     layer.trainable = False
-
-  x1 = tf.keras.layers.Activation('relu', name='relu_conv1')(x1)
-  x1 = Dropout(0.5)(x1)
-  x1 = BatchNormalization(axis=1)(x1)
-  x1 = tf.keras.layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool1')(x1)
-  x1 = Dropout(0.5)(x1)
-  x1 = BatchNormalization(axis=1)(x1)
-  x1 = tf.keras.layers.Convolution2D(3, 1, 1, name='conv2')(x1)
-  x1 = Dropout(0.5)(x1)
-  x1 = BatchNormalization(axis=1)(x1)
-  x1 = tf.keras.layers.Activation('relu', name='relu_conv2')(x1)
-  x1 = Dropout(0.5)(x1)
-  x1 = BatchNormalization(axis=1)(x1)
-  x1 = tf.keras.layers.GlobalAveragePooling2D()(x1)
-  x1= Dense(512, activation= 'tanh')(x1)
-  print("test2")
+  x1 = tf.keras.layers.GlobalMaxPooling2D()(x1)
+  x1 = tf.keras.layers.Dense(512, activation='relu')(x1)
   print("shape of x1",x1.shape)
   # The question network
   q_input = Input(shape=(768,))
   x2 = Dense(512, activation='tanh')(q_input)
-  x2 =Dropout(0.5)(x2)
-  x2 = BatchNormalization(axis=1)(x2)
   x2 = Dense(512, activation='tanh')(x2)
   print("shape of x2",x2.shape)
   print("type of x1 and x2", type(x1), type(x2))
 
   def attention(x1, x2, dropout=True):
-    
-    # img = tf.nn.tanh(tf.compat.v1.layers.dense(image_tensor , out_dim))
-    
-    # ques = tf.nn.tanh(tf.compat.v1.layers.dense(question_tensor , out_dim))
-    
-    # ques = tf.expand_dims(ques , axis = -2)
     
     IQ = tf.nn.tanh(x1 + x2)
 
@@ -78,7 +40,7 @@ def build_model(im_shape, vocab_size, num_answers, big_model):
         IQ = tf.nn.dropout(IQ , rate=1 - (0.5))
     
     temp = Dense(1)(IQ)
-    #temp = Dropout(0.5)(temp)
+    
     temp = tf.reshape(temp , [-1,temp.shape[1]])
     print("temp shpe: ", temp.shape)
     
@@ -98,23 +60,14 @@ def build_model(im_shape, vocab_size, num_answers, big_model):
     
   att_l2 , att = attention(x1, att)
     
-  att = tf.nn.dropout(att , rate=1 - (0.4))
-    
-  # att = Dense(768)(att)
-  # print("att shpe: ", att.shape)
-    
-  # att = tf.nn.softmax(att)
+  att = tf.nn.dropout(att , rate=1 - (0.4)
   print("att shpe: ", att.shape)
     
   print(att.shape)
     
   attention_layers = [att_l1 , att_l2]
 
-  # Merge -> output
-  # out = Multiply()([x1, x2])
   out = Dense(512, activation='tanh')(att)
-  out = Dropout(0.5)(out)
-  out = BatchNormalization(axis=1)(out)
   out = Dense(num_answers, activation='softmax')(out)
   print("out shape: ", out.shape)
   print("im_input shpe: ", im_input.shape)
@@ -123,5 +76,3 @@ def build_model(im_shape, vocab_size, num_answers, big_model):
   model.compile(Adam(lr=5e-4), loss='categorical_crossentropy', metrics=['accuracy'])
 
   return model
-
-
